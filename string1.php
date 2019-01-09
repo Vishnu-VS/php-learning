ee9f61097a5b8087356b35435bc5a28ed6523ae3
Vishnu-VS <vishnu.vs@yahoo.com>
2019-01-09 12:40:17 +0530

    String operations

---
+++ string1.php
@@ -0,0 +1,8 @@
+<?php
+    $string1="This will print";
+    $string2='string';
+    print("$string1");
+    print($string2);
+    $string2="This $string1 must print now";
+    print("$string2");
+?>
