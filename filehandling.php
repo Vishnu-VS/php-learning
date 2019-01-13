c4b227bdbc6aca567d2a7be67d689f25b3df616d
Vishnu-VS <vishnu.vs@yahoo.com>
2019-01-13 19:57:25 +0530

    file handling operatoins

--- filehandling.php
+++ filehandling.php
@@ -1,3 +1,13 @@
 <?php
-    echo readfile("webdictionary.txt");
+    //echo readfile("webdictionary.txt");
+    $myfile=fopen("webdictionary.txt","r") or die("Unable to open file!");
+    //echo fread($myfile,filesize("webdictionary.txt"));
+    while(!feof($myfile)){
+        echo fgetc($myfile);
+    }
+    fclose($myfile);
+    $myfile=fopen("testfile.txt","a");
+    $txt="John Doe\n";
+    fwrite($myfile,$txt);
+    fclose($myfile);
 ?>
