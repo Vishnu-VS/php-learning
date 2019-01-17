2fb8e7ad5d6e15fa4b202dac572962e8b9a1fd9f
Vishnu-VS <vishnu.vs@yahoo.com>
2019-01-17 15:28:53 +0530

    Custom Exception

--- exception_handling.php
+++ exception_handling.php
@@ -1,21 +1,31 @@
 <?php
-    function checkNum($number){
+    /*function checkNum($number){
         if($number>1){
             throw new Exception("Value must be 1 or below");
         }
         return true;
+    }*/
+    class customException extends Exception{
+        public function errorMessage(){
+            //err msg
+            $errorMsg='Error on line '.$this->getLine().'in '.$this->getFile().' :<b>'.$this->getMessage().'</b> is not valid E-Mail address';
+            return $errorMsg;
+        }
     }
-
     //trigger exception in a try block
+    $email="someone@example...com";
     try{
-        checkNum(2);
+        //checkNum(2);
         //If the exception is thrown the following line will not be shown
-        echo "This line shows when there is no exception";
+        //echo "This line shows when there is no exception";
+        if(filter_var($email,FILTER_VALIDATE_EMAIL)===FALSE){
+            throw new customException($email);
+        }
     }
 
     //catch block
-    catch(Exception $e){
-        echo 'Message: '.$e->getMessage();
+    catch(customException $e){
+        echo $e->errorMessage();
     }
     //checknum(3);
 ?>
