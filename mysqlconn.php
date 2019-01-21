cf29fcd15aaebd78d119ecf6dacf909bd1da132a
Vishnu-VS <vishnu.vs@yahoo.com>
2019-01-21 15:01:54 +0530

    Created tables

--- mysqlconn.php
+++ mysqlconn.php
@@ -1,10 +1,11 @@
 <?php
     $servername="localhost";
-    $username="username";
-    $password="password";
+    $username="root";
+    $password="";
+    $dbname="myDB";
 
     //create connection
-    $conn=new mysqli($servername,$username,$password);
+    $conn=new mysqli($servername,$username,$password,$dbname);
 
     //check connection
     if($conn->connect_error){
@@ -12,12 +13,20 @@
     }
     echo "Connected succesfully";
 
-    //create database
-    $sql="CREATE DATABASE myDB";
+    //create table
+    $sql="CREATE TABLE MyGuests(
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstname VARCHAR(30) NOT NULL,
+        lastname VARCHAR(30) NOT NULL,
+        email VARCHAR(50),
+        reg_date TIMESTAMP
+        )";
+
     if($conn->query($sql)===TRUE){
-        echo "Database created succesfully";
+        echo "Table MyGuests created succesfully";
     }else{
-        echo "Error creating database ".$conn->error;
-    }
+        echo "Error creating tables ".$conn->error;
+    } 
+
     $conn->close();
 ?>
